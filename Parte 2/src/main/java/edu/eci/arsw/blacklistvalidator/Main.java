6ee0c69bb42e3618a674a9706bb10a200f27f950
ricardo.pulido <rpulidor17.rpr18@gmail.com>
2023-09-02 09:08:39 -0500

    Corrección archivos parte 2

--- Parte 2/src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ Parte 2/src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -24,30 +23,3 @@ public class Main {
     }
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package edu.eci.arsw.blacklistvalidator;
-
-import java.util.List;
-
-/**
- *
- * @author hcadavid
- */
-public class Main {
-
-
-    private static Runtime runtime= Runtime.getRuntime();
-    public static void main(String a[]){
-        HostBlackListsValidator hblv=new HostBlackListsValidator();
-        List<Integer> blackListOcurrences=hblv.checkHost("202.24.34.55",  100);
-        System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
-
-    }
-    
-}
->>>>>>> 7d999a58bf93b9aa7d3d736ee230140de5e4e631
