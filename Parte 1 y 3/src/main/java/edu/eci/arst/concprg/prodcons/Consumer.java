127bfe15a804f34c4ffe6525e56fac853b503e82
lgar000 <laura.garcia-a@mail.escuelaing.edu.co>
2023-09-07 11:04:28 -0500

    parte1.1 y 1.2

--- Parte 1 y 3/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ Parte 1 y 3/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -23,11 +23,13 @@ public class Consumer extends Thread{
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+            synchronized (queue){
+                if (queue.size() > 0) {
+                    int elem=queue.poll();
+                    System.out.println("Consumer consumes "+elem);
+                }
             }
+
             
         }
     }
--- Parte 1 y 3/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ Parte 1 y 3/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -31,17 +31,17 @@ public class Producer extends Thread {
     @Override
     public void run() {
         while (true) {
+            synchronized (queue){
+                dataSeed = dataSeed + rand.nextInt(100);
+                System.out.println("Producer added " + dataSeed);
+                queue.add(dataSeed);
 
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-
         }
     }
 }
